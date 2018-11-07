16b6bb75b98eb86c47ebf052fd90b2f58bedd10a
Lane Kersten <>
2018-11-06 19:17:23 -0500

    Added swiftyJson into download function to make parsing easier

--- MoodGrid/MoodGrid/ViewController.swift
+++ MoodGrid/MoodGrid/ViewController.swift
@@ -16,14 +16,18 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     @IBOutlet weak var collectionView: UICollectionView!
     
     //variables
-    var pictures = [String]()
+    var pictures = [PictureObject]()
     let apiKey = "ac21996467c394e3c5e74768bfbb3de744913d53e81ec4ca425a7558c3ef3388"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        //register xib
         collectionView.register(UINib(nibName: "PictureViewCell", bundle: nil), forCellWithReuseIdentifier: "PictureCell")
+        
+        //grab pictures from API
+        downloadPicturesFromUnsplash()
     }
 
     //MARK: CollectionView Callbacks
@@ -46,11 +50,26 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     func downloadPicturesFromUnsplash() {
         //alamofire request
-        Alamofire.request("https://api.unsplash.com/photos?page=1&client_id=\(apiKey)").responseJSON { (response) in
-            print(response)
-            
-            if let jsonObj = response.result.value {
-                let json = JSON(jsonObj)
+        Alamofire.request("https://api.unsplash.com/photos?page=1&client_id=\(apiKey)").validate().responseJSON { response in
+            //check if request was successful
+            switch response.result {
+            case .success(let value):
+                
+                //creat a swiftyJson object
+                let json = JSON(value)
+                
+                //loop through resulting json
+                for (index, subJson): (String, JSON) in json {
+                    //create picture objects from the response and store them in pictures var
+                    
+                    
+                }
+                
+                //print json to debug
+                print("JSON: \(json)")
+            case .failure(let error):
+                //print error
+                print(error)
             }
         }
     }
